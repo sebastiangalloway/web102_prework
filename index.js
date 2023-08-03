6aa75dc886b06beac5009069c9136b229ff89e53
Sebastian Galloway <sgalloway111@gmail.com>
2023-08-03 02:19:40 -0400

    games for loop added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
 
@@ -44,7 +44,7 @@ function addGamesToPage(games) {
 
 
         // append the game to the games-container
-
+    }
 }
 
 // call the function we just defined using the correct variable
